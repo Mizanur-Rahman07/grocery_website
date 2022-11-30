c823ce7f618a7b4ebc9ea82db93ba87e62a39f89
minhaj <minhaj.ama@gmail.com>
2022-11-30 20:48:55 +0600

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
+//  ------------ Inspact Off -------------------------//
+    // (document.onkeydown = function (e) {
+    //   return (
+    //     123 != event.keyCode &&
+    //     (!e.ctrlKey || !e.shiftKey || e.keyCode != "I".charCodeAt(0)) &&
+    //     (!e.ctrlKey || !e.shiftKey || e.keyCode != "C".charCodeAt(0)) &&
+    //     (!e.ctrlKey || !e.shiftKey || e.keyCode != "J".charCodeAt(0)) &&
+    //     (!e.ctrlKey || e.keyCode != "U".charCodeAt(0)) &&
+    //     void 0
+    //   );
+    // })
+
+    // window.addEventListener(
+    //   "contextmenu",
+    //   function (e) {
+    //     e.preventDefault();
+    //   },
+    //   !1
+    // )
+//  ------------ Inspact Off -------------------------//
+
 
 // A $( document ).ready() block.
 // $(document).ready(function () {
@@ -29,6 +50,15 @@ $('.responsive').slick({
   slidesToScroll: 1,
   responsive: [
     {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
@@ -38,7 +68,7 @@ $('.responsive').slick({
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 768,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1
@@ -56,3 +86,41 @@ $('.responsive').slick({
     // instead of a settings object
   ]
 });
+
+// ----------- Recent Articel for Read More Code------------//
+  function myFunction(id,myBtn) {
+
+    const btnText = document.getElementById(myBtn);
+
+    var x = document.getElementById(id);
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      btnText.innerHTML = "Read More";
+    } else {  0
+      x.style.display = "block";
+      btnText.innerHTML = "Read less"; 
+    }
+
+    // if (recentArticelDots.style.display === "none") {
+    //   recentArticelDots.style.display = "inline";
+    //   btnText.innerHTML = "Read More"; 
+    //   moreText.style.display = "none";
+    // } else {
+    //   recentArticelDots.style.display = "none";
+    //   btnText.innerHTML = "Read less"; 
+    //   moreText.style.display = "inline";
+    // }
+  }
+ // ----------- Recent Articel for Read More Code------------//                
+
+//  ------------Scroll Sticky Function-----------------------//
+ $(window).on('scroll', function () {
+  var scroll = $(window).scrollTop();
+  if (scroll < 50) {
+    $("#Menu").removeClass("sticky");
+  } else {
+    $("#Menu").addClass("sticky");
+  }
+});
+//  ------------Scroll Sticky Function-----------------------//
+
